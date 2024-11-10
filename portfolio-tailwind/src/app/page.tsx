206271a26c229a606a59f2b9b8a6886e52cadf37
Sadia Asif Khan <sadia.rubab1988@gmail.com>
2024-11-10 20:13:38 +0500

    Update page.tsx

--- portfolio-tailwind/src/app/page.tsx
+++ portfolio-tailwind/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Image from "next/image"
-import sadia from "../../image/sadia.png"
+import sadia from "../../image/sadia.jpg"
 export default function home(){
   return(
     <div>
